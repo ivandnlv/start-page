5ea45050588bbc9a633984991222c8f9d1a6ade9
Ivan <daniloffivan0@gmail.com>
2023-10-30 21:40:26 +0500

    remove weather

--- script.js
+++ script.js
@@ -43,50 +43,50 @@ function monthToString(month) {
   return MONTHS[Number(month)];
 }
 
-async function fetchWeatherData() {
-  try {
-    const API_KEY = '8a84eee777714351b6d34610233010';
-    const URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=Salavat`;
+// async function fetchWeatherData() {
+//   try {
+//     const API_KEY = '8a84eee777714351b6d34610233010';
+//     const URL = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=Salavat`;
 
-    const data = await fetch(URL);
+//     const data = await fetch(URL);
 
-    const items = await data.json();
+//     const items = await data.json();
 
-    return items;
-  } catch (error) {
-    return error;
-  }
-}
+//     return items;
+//   } catch (error) {
+//     return error;
+//   }
+// }
 
 window.addEventListener('DOMContentLoaded', () => {
-  const weather = fetchWeatherData();
+  // const weather = fetchWeatherData();
 
-  const weatherImageEl = document.getElementById('weather-image');
-  const weatherTextEl = document.getElementById('weather-text');
-  const weatherFeelsEl = document.getElementById('weather-feels-like');
-  const weatherCity = document.getElementById('city');
+  // const weatherImageEl = document.getElementById('weather-image');
+  // const weatherTextEl = document.getElementById('weather-text');
+  // const weatherFeelsEl = document.getElementById('weather-feels-like');
+  // const weatherCity = document.getElementById('city');
 
-  weather
-    .then((data) => {
-      const {
-        current: {
-          condition: { icon, text },
-          temp_c,
-          feelslike_c,
-        },
-        location: { name },
-      } = data;
-      weatherCity.textContent = name;
-      weatherImageEl.src = 'https:' + icon;
-      weatherImageEl.alt = text;
-      weatherTextEl.textContent = `Температура: ${Math.floor(temp_c)}°`;
-      weatherFeelsEl.textContent = `Ощущается как: ${Math.floor(feelslike_c)}°`;
-    })
-    .catch((reason) => {
-      weatherImageEl.style.display = 'none';
-      weatherTextEl.textContent = reason;
-      weatherTextEl.style.color = 'red';
-    });
+  // weather
+  //   .then((data) => {
+  //     const {
+  //       current: {
+  //         condition: { icon, text },
+  //         temp_c,
+  //         feelslike_c,
+  //       },
+  //       location: { name },
+  //     } = data;
+  //     weatherCity.textContent = name;
+  //     weatherImageEl.src = 'https:' + icon;
+  //     weatherImageEl.alt = text;
+  //     weatherTextEl.textContent = `Температура: ${Math.floor(temp_c)}°`;
+  //     weatherFeelsEl.textContent = `Ощущается как: ${Math.floor(feelslike_c)}°`;
+  //   })
+  //   .catch((reason) => {
+  //     weatherImageEl.style.display = 'none';
+  //     weatherTextEl.textContent = reason;
+  //     weatherTextEl.style.color = 'red';
+  //   });
 
   setInterval(timeAndDateUpdate, 1000);
 
